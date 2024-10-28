2bae55e133d750670aa9fffe35d7c2df41f7a733
Radmir <galiullinradmir17@gmail.com>
2024-10-28 20:09:29 +0300

    override PerevalSerializer.to_representation (comments in function)

--- app/perevals/serializers.py
+++ app/perevals/serializers.py
@@ -23,18 +23,19 @@ class PerevalLevelSerializer(serializers.ModelSerializer):
         
         model = PerevalLevel
         fields = ('winter', 'summer', 'autumn', 'spring')
+        
 
 class PerevalSerializer(serializers.ModelSerializer):
 
     user = UserProfileSerializer()
     coords = CoordinatesSerializer()
     level = PerevalLevelSerializer()
-    images = serializers.ListField(child=serializers.DictField())
+    images = serializers.ListField()
 
     class Meta:
         
         model = Pereval
-        fields = ('beauty_title','title', 'connect', 'add_time', 'user', 'coords', 'level', 'images')
+        fields = ('beauty_title', 'title', 'connect', 'add_time', 'user', 'coords', 'level', 'images')
 
 
     # проверка ключей, для последующей подготовки, 
@@ -87,7 +88,47 @@ class PerevalSerializer(serializers.ModelSerializer):
                     )
             
         return instance
+    
+
+    
+    def to_representation(self, instance):
+
+        data = {}
+
+        # берем все поля, включая status из модели перевал
+        fields = instance._meta.get_fields()
+        for field in fields:
+                
+            # так как images явно не указана в модели Pereval, 
+            # попытка взять название приводит к AttributeError
+            try:
+
+                # _id оканчивается у oneToOne и foreign связей
+                if field.attname.endswith('_id'):
+                    field_name = field.attname.replace('_id', '') #меняем название с field_id на field
+                    # берем класс сериализатора из self.fields.fields и получаем данные
+                    data[field_name] = self.fields.fields[field_name].to_representation(instance=getattr(instance, field_name))
+                
+                else:
+                    
+                    data[field.attname] = getattr(instance, field.attname)
+
+            # ловим поле images
+            except AttributeError:
+                
+                images = []
+                
+                # достаем все обьекты и апендим в список
+                for image in instance.images.all():
+                    images.append({
+                        'title': image.title,
+                        'data': image.image.url
+                    })
+        
+        # добавляем в конце, так красивее
+        data['images'] = images
+        
+        return data
+
             
-
-
 
