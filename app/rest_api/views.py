fcdd5305df4f7c0e8ce41abefded0fc26926800e
Radmir <galiullinradmir17@gmail.com>
2024-10-29 01:13:00 +0300

    add patch request logic

--- app/rest_api/views.py
+++ app/rest_api/views.py
@@ -1,7 +1,9 @@
+from django.forms import ValidationError
 from rest_framework import response, status
 from rest_framework.decorators import api_view
 
 from perevals.serializers import PerevalSerializer
+from django.core.exceptions import BadRequest
 
 
 
@@ -11,14 +13,41 @@ def getedit_pereval(request, pk):
     if request.method == 'GET':
 
         try:
-            pereval = PerevalSerializer.Meta.model.objects.prefetch_related('images').select_related('coords', 'level', 'user').get(pk=pk)
+            pereval = PerevalSerializer.Meta.model.objects.get(pk=pk)
         except PerevalSerializer.Meta.model.DoesNotExist:
-            return  response.Response(data={'pereval': None}, status=status.HTTP_404_NOT_FOUND)
+            return  response.Response(data={'pereval': None, 'message': f'Object with id {pk}. Does not exist'}, status=status.HTTP_404_NOT_FOUND)
         
         serializer = PerevalSerializer(pereval)
         return  response.Response(data={'pereval': serializer.data}, status=status.HTTP_200_OK)
-        ...
 
+    else:
+
+        try:
+
+            try:
+            
+                pereval = PerevalSerializer.Meta.model.objects.select_related('user','coords','level').get(pk=pk)
+                serializer = PerevalSerializer(instance=pereval, data=request.data)
+            
+            except PerevalSerializer.Meta.model.DoesNotExist:
+                return  response.Response(data={'state': 0, 'message': f'Object with id {pk}. Does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        
+            if serializer.is_valid():
+
+                serializer.save()
+                return response.Response(data={'state': 1, 'message': None}, status=status.HTTP_200_OK)
+            
+            else:
+                return response.Response(data={'state': 1, 'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        except BadRequest as ex:
+            return response.Response(data={'state': 0, 'message': str(ex)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as ex:
+            return response.Response(data={'state': 0, 'message': str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
 
 @api_view(['POST'])
 def submitData(request):
@@ -30,19 +59,15 @@ def submitData(request):
         if serializer.is_valid():
 
             pereval = serializer.save(**request.data)
-            resp = response.Response(data={'status': 200, 'message': None, 'id': pereval.id}, status=status.HTTP_201_CREATED)
-
+            return response.Response(data={'status': 200, 'message': None, 'id': pereval.id}, status=status.HTTP_201_CREATED)
         
         else:
 
-            resp = response.Response(data={'status': 400, 'message': serializer.errors, 'id': None}, status=status.HTTP_400_BAD_REQUEST)
+            return response.Response(data={'status': 400, 'message': serializer.errors, 'id': None}, status=status.HTTP_400_BAD_REQUEST)
 
     except Exception as ex:
 
-        resp = response.Response(data={'status': 500, 'message': str(ex), 'id': None}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response.Response(data={'status': 500, 'message': str(ex), 'id': None}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    finally:
-
-        return  resp
         
         
